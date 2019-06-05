c846031f0ac6b70eea3a4abfd7b421935645a8fa
Saibal Roy <=>
2019-06-05 15:22:13 +0530

    laravel telescope istalled for queue driver set to database

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/', function () {
     /** Using Job Class */
     /** Important note : If the job class don't implement ShouldQueue then it will behave as sync and will not use the queue connection driver and no entries thus made to the jobs table.
      * One more thing if laravel horizon is being used when queue connection driver is being redis.
+     * But with telescope using driver as database also works
      */
     dispatch(new \App\Jobs\LogUser);
     // return view('welcome');
